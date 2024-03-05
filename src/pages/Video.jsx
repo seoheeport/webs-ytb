a770ff59c51f3b0ef89e85b679b7289df5a7c935
seoheeport <seoheeport05@gmail.com>
2024-03-05 16:31:12 +0900

    git

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { fetchFromAPI } from '../utils/api';
 
 import Main from '../components/section/Main';
 import ReactPlayer from 'react-player'
